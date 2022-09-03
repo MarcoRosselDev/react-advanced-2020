c1e8152784fdc7d011dbaa2fec825692d5699849
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 15:19:31 -0400

    memory practice 2

---
+++ src/tutorial/paractis/2.js
@@ -0,0 +1,12 @@
+/*
+      <h2>memory practice list to do</h2>
+      <input type="text" class="ipt" />
+      <button class="btn-put btn">put</button>
+      <button class="btn-clean btn">clean</button>
+      <div class="list"></div>
+*/
+
+const ipt = document.querySelector(".ipt");
+const btnPut = document.querySelector(".btn-put");
+const btnClean = document.querySelector(".btn-clean");
+const listDiv = document.querySelector(".list");
