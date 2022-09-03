ce533f11b3d29ab0653635064f81adb4f31ff5e7
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 16:26:11 -0400

    memory practis 3.2

--- src/tutorial/paractis/3.js
+++ src/tutorial/paractis/3.js
@@ -2,9 +2,19 @@ import React, { useState } from "react";
 
 const RandomThing = () => {
   const [texto, setTexto] = useState("random title 1");
+  const changeTitle = () => {
+    if (texto === "random title 1") {
+      setTexto("anoter title");
+    } else {
+      setTexto("random title 1");
+    }
+  };
   return (
     <React.Fragment>
       <h2>{texto}</h2>
+      <button type="button" className="btn" onClick={changeTitle}>
+        click
+      </button>
     </React.Fragment>
   );
 };
