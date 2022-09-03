45a212a52c4bb14cfe5327d8b7b1727ff43c3c07
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 15:08:12 -0400

    example 2 practis finished

--- src/tutorial/paractis/1.js
+++ src/tutorial/paractis/1.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 
 const RandomTitle = () => {
-  let title = "random title";
   const [texto, setTexto] = useState("random text from useState");
+  const change = () => {
+    if (texto === "random text from useState") {
+      setTexto("another random title");
+    } else {
+      setTexto("random text from useState");
+    }
+  };
   return (
     <React.Fragment>
-      <h2>{title}</h2>
+      <h2>{texto}</h2>
+      <button type="button" className="btn" onClick={change}>
+        click
+      </button>
     </React.Fragment>
   );
 };
