7360c1bfdd55d12fd7ec49897d676a9c802c5b99
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 15:48:38 -0400

    memory practice 2.2

--- src/tutorial/paractis/2.js
+++ src/tutorial/paractis/2.js
@@ -1,4 +1,4 @@
-/*
+/* form from 2.html
       <h2>memory practice list to do</h2>
       <input type="text" class="ipt" />
       <button class="btn-put btn">put</button>
@@ -10,3 +10,20 @@ const ipt = document.querySelector(".ipt");
 const btnPut = document.querySelector(".btn-put");
 const btnClean = document.querySelector(".btn-clean");
 const listDiv = document.querySelector(".list");
+let putArr = [];
+
+// listen to the click event
+
+btnPut.addEventListener("click", function () {
+  putArr.push(ipt.value);
+  ipt.value = "";
+  printDom(putArr);
+});
+
+function printDom(arr) {
+  let printHtml = "";
+  for (let i = 0; i < arr.length; i++) {
+    printHtml += `<li>${arr[i]}</li>`;
+  }
+  listDiv.innerHTML = printHtml;
+}
