d425db88c56719ff320cd5ef2672f1bfaafb5a03
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 20:38:46 -0400

    example 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/2-useState-basics";
-import Final from "./tutorial/1-useState/final/2-useState-basics";
-import RandomTitle from "./tutorial/paractis/1";
-import RandomThing from "./tutorial/paractis/3";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 function App() {
   return (
     <div className="container">
-      {/* <Setup /> */}
-      {/* <RandomTitle /> */}
-      {/* <Final /> */}
-      <RandomThing />
+      <Setup />
     </div>
   );
 }
