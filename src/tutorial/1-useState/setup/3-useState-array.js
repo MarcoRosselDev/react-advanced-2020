35a6730433871d0523f753bb3ec84de5bdc4281a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-03 20:46:55 -0400

    example 3.2

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // usar React.useState funciona = pero evita importarlo from react
+  const [people, setPeople] = React.useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
