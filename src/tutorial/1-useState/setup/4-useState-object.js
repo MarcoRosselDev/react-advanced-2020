0ccda63b3d39f8d9ec54a965de0cecf50ca2dd6e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-04 20:28:50 -0300

    example 4

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
+  const [person, usePerson] = useState({
+    name: "marco",
+    age: 29,
+    message: "random message",
+  });
+  console.log(person);
   return <h2>useState object example</h2>;
 };
 
