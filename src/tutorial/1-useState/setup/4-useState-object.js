d5278b475dd1bf4e4724bdefdf4efc2b5a1d0bb6
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-04 20:54:13 -0300

    example 4.3

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,14 +6,22 @@ const UseStateObject = () => {
     age: 29,
     message: "random message",
   });
+
+  const [name, setName] = useState("marcus");
+  const [age, setAge] = useState(30);
+  const [message, setMessage] = useState("mensaje random de ejemplo");
+
   const changeMessage = () => {
-    setPerson({ ...person, message: "another message" });
+    // setPerson({ ...person, message: "another message" });
   };
   return (
     <>
-      <h3>{person.name}</h3>
+      {/* <h3>{person.name}</h3>
       <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{person.message}</h3> */}
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className="btn" onClick={changeMessage}>
         change message
       </button>
