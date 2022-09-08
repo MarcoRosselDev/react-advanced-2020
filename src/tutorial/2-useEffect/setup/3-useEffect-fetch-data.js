6099e8b00b7df40529a7e89a3e11fcbad21d98e0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-07 22:40:32 -0400

    useEffec 3.1

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
   return <h2>fetch data</h2>;
 };
 
