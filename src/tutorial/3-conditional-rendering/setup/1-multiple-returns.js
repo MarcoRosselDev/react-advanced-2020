145acc661bd4e58b6ba66356a680747084a05708
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-14 05:54:58 -0300

    conditional rendering 1

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,12 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
   return <h2>multiple returns</h2>;
 };
 
