e8aeb7d7d4f095b93f33514bbc653e8bad7e71e1
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-13 23:29:03 -0300

    reminder-2 3

---
+++ src/tutorial/2-useEffect/final/reminder-2/index.js
@@ -0,0 +1,13 @@
+/*
+    <div class="container">
+      <h1>list todo</h1>
+      <input type="text" class="input" />
+      <button class="btn-put">put</button>
+      <button class="btn-clean">clean</button>
+      <div class="list"></div>
+    </div>
+*/
+
+const input = document.querySelector(".input");
+const btnPut = document.querySelector(".btn-put");
+const btnClean = document.querySelector(".btn-clean");
