cfc37cfc5619a5fe3b00ea394e50f010bebe019e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-18 13:31:30 -0300

    short circuit 1.2

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,14 +4,13 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  const [isError, setIsError] = useState(false);
 
   return (
     <>
       <h1>{text || "marco rossel"}</h1>
-      {text && <h1>hola mundo</h1>}
-      {!text && <h1>hola mundo</h1>}
+      <button className="btn">toggle error</button>
+      {isError && <h1>Error...</h1>}
     </>
   );
 };
